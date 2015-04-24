d8dd1624511de8de00f7a6cc5baa7c42d7490a81
Oleksii <byggerr@gmail.com>
2015-04-24 10:53:13 +0300

    Update Task7.java

--- HomeWork3/Task7.java
+++ HomeWork3/Task7.java
@@ -1,27 +1,20 @@
-
 import java.util.Scanner;
-/**
- * Created by Oleksii on 22.04.2015.
- */
-public class Task7 {
-    public static void main(String[] args) {
+
+public class HelloWorld{
+
+     public static void main(String []args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int n1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int n2 = scanner.nextInt();
-        average(n1, n2);
-       // System.out.println("Average = " + (int) average(n1, n2));
-    }
+        System.out.print("Average = " + average(n1, n2));
+        
+        scanner.close();
+     }
 
-//    private static int average(int n1, int n2) {
-//        long sum = n1+n2;
-//        return (int)(sum/2);
-//    }
-    private static void average(int n1, int n2) {
-        double sum = (n1/10 + n2/10);
-        int average = (int) (sum*10/2);
-        System.out.println("Average = " + average);
+    private static int average(int n1, int n2) {
+       return (n1/2+n2/2) + (n1 % 2 + n2 % 2)/2;
 
     }
 }
